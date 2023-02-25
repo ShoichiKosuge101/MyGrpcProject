b2cb337d478bb0a1ea60890506244b9563bd4e5d
ShoichiKosuge101 <aesgaaard@gmail.com>
2023-02-25 18:59:55 +0900

    Client proj add

---
+++ GrpcGreeterClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcGreeterClient;
+
+// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+var server = "https://172.24.128.1:7094";
+var option = new GrpcChannelOptions()
+{
+    HttpClient = new HttpClient(new HttpClientHandler
+    {
+        ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator,
+    })
+};
+
+var channel = GrpcChannel.ForAddress(server, option);
+var client = new Greeter.GreeterClient(channel);
+
+var reponse = await client.SayHelloAsync(new HelloRequest{Name = "World"});
